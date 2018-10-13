5e2ec221a62366f33316baf514879a8dec69eaab
James <james.randolph2016@gmail.com>
2018-10-13 12:20:26 -0400

    added validations and relationship o users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-        
+  
+  validates :username, presence: :true
+  validates :email, presence: true
+
+  has_secure_password
+
+  has_many :books
+  has_many :reviews
+
 end
