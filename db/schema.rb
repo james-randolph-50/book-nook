34197da982aad1c00c10f47f8444e53dd47a29b0
James <james.randolph2016@gmail.com>
2018-10-13 10:56:53 -0400

    removed extra db add devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181009225551) do
+ActiveRecord::Schema.define(version: 20181013144227) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
